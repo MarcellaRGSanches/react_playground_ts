939155c0b3be7a41a54d38ebdd800ab1d75c5d3e
MarcellaRGSanches <rochaamarcellaaa@gmail.com>
2024-08-22 17:08:03 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+import './App.css'
+import Contador from './pages/contador/Contador'
+import Tarefa from './pages/tarefa/Tarefa'
+// import Home from './pages/home/Home'
+
+function App() {
+  // const [nome_estado, função_setState] = useState(valor_inicial_do_estado)
+  return (
+    <>
+      {/* <Home 
+        titulo='Minha primeira postagem'
+        texto='Texto da minha primeira postagem'
+        /> */}
+      {/* <Contador /> */}
+      <Tarefa />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,23 @@
+﻿import { useState } from "react"
+
+function Contador() {
+  // const [nome_estado, função_setState] = useState(valor_inicial_do_estado)
+
+  //Estado Valor (React)
+  const [valor, setValor] = useState(0);
+  //Function (Typescript)
+  function handleClick() {
+    setValor(valor + 1);
+  }
+  //Dentro do return (HTML E CSS)
+  return (
+    <div>
+
+      <h2>Componente Contador</h2>
+      <p>O valor atual do meu estado é: {valor}</p>
+      <button onClick={handleClick}>Adicionar +1</button>
+    </div>
+  )
+}
+
+export default Contador
